db435b8621fb77fc8ad0c121d9623629a95ab682
sadsoap <146782663+sadsoap@users.noreply.github.com>
2024-05-06 12:45:00 -0700

    implement explore section and respond to Check your understanding
    Co-authored-by: lindseyrapp <lindseyrapp@users.noreply.github.com>

--- README.md
+++ README.md
@@ -2,3 +2,18 @@
 ## Lindsey Rappaport and Sophia Davis
 
 * [Expose - Party Horn](https://sadsoap.github.io/Lab5_Starter/expose.html)
+
+<!-- 1) Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.   
+2) Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters. -->
+### Check your understanding
+#### Lindsey R.
+1. No, I would not use a unit test for the "message" feature because unit tests should be used on individual components and this has multiple in.  
+2. Yes, I would use a unit test to test the "max message length" feature. You could check that the number of characters are less than or equal to 80 since this is an indiviual component.  
+
+
+#### Sophia D.
+1. No, since unit testing cannot test how these individual components interact with each other on an application/feature level. 
+2. Yes, since unit testing can apply to something small, such as the character/word limit in a user's message textbox. 
+
+
+
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,53 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  
+  //initialize elements from html into js
+  const faceImg = document.querySelector("img[src='assets/images/smiling.png']");
+  const textBox = document.getElementById("text-to-speak");
+  const voiceSel = document.getElementById("voice-select");
+  const talkButton = document.querySelector("button");
+  const synthesizer = window.speechSynthesis;
+
+  console.log('test');
+
+  //Load voices and populate dropdown and select
+  window.addEventListener("load", (event) => {
+    
+    // code to run when the event is triggered
+   const voices = speechSynthesis.getVoices();
+
+    voices.forEach((element)=>{
+
+      const option = document.createElement("option");
+      option.textContent = element.name;
+      voiceSel.appendChild(option);
+  
+    });
+  });
+
+  //input for textbox
+  textBox.addEventListener("input", (event) => {
+    const text = event.target.value;
+  });
+
+  //button click, play respective voice
+  talkButton.addEventListener("click", (event) => {
+    const selectedVoice = voiceSel.value;
+    const textToSpeak = textBox.value;
+    const utterance = new SpeechSynthesisUtterance(textToSpeak);
+    
+    const voices = speechSynthesis.getVoices();
+    const selectedVoiceObject = voices.find(voice => voice.name === selectedVoice);
+    utterance.voice = selectedVoiceObject;
+    
+    utterance.onstart = () => {
+      faceImg.src = "assets/images/smiling-open.png";
+    };
+    utterance.onend = () => {
+      faceImg.src = "assets/images/smiling.png";
+    };
+    speechSynthesis.speak(utterance);
+  });
+
 }
